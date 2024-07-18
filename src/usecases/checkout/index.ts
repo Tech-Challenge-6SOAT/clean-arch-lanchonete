87fdff0ca862be6f0fa69fe3b6e2e699e45ebd02
Danilo Cassola <danilocassola@gmail.com>
2024-07-17 23:14:35 -0300

    chore: add _formatarProdutosPedido method in CheckoutUseCase

--- src/usecases/checkout/index.ts
+++ src/usecases/checkout/index.ts
@@ -7,6 +7,7 @@ import { CPF } from "../../value-objects/cpf";
 import { PedidoUseCase } from "../pedido";
 
 export class CheckoutUseCase {
+    // private _cliente: Cliente | undefined;
 
     constructor(
         private readonly pedidoUseCase: PedidoUseCase,
@@ -15,45 +16,45 @@ export class CheckoutUseCase {
     ) { }
 
     async checkout({ produtos, cpf }: { produtos: { id: string, quantidade: number }[], cpf: string }): Promise<string> {
-        const clienteEncontrado = await this.clienteGateway.buscarCliente({ cpf: new CPF(cpf) });
-        if (!clienteEncontrado) {
-            throw new Error('Cliente não encontrado')
+        let cliente;
+        if (cpf) {
+            const clienteEncontrado = await this.clienteGateway.buscarCliente({ cpf: new CPF(cpf) });
+            if (!clienteEncontrado) {
+                throw new Error('Cliente não encontrado')
+            }
+            cliente = new Cliente(clienteEncontrado.id, clienteEncontrado.nome, clienteEncontrado.email, clienteEncontrado.cpf)
         }
-        const cliente = new Cliente(clienteEncontrado.id, clienteEncontrado.nome, clienteEncontrado.email, clienteEncontrado.cpf)
-        const { produtosPedido, totalPedido } = await this._calcularTotalPedido(produtos)
 
-        const produtosCriados = produtosPedido.map(({ produto, quantidade }) => {
-            return { produto: new Produto(produto.id, produto.categoria, produto.nome, produto.preco, produto.descricao), quantidade }
-        })
+        const produtosPedido = await this._formatarProdutosPedido(produtos)
+        const totalPedido = this._calcularTotalPedido(produtosPedido)
 
         const pedidoCriado = await this.pedidoUseCase.criar({
             cliente,
-            produtos: produtosCriados,
+            produtos: produtosPedido,
             total: totalPedido,
             status: new Status('Recebido'),
-            senha: String(Math.floor(Math.random() * 10000)),
+            senha: String(Math.floor(Math.random() * 10000)).padStart(4, '0'),
         })
+
+        // TO DO: Processar pagamento
+        // TO DO: Atualizar status do pagamento do pedido
 
         return pedidoCriado.senha
     }
 
-    private async _calcularTotalPedido(produtos: { id: string, quantidade: number }[]): Promise<
-    {
-        produtosPedido: { produto: Produto, total: number, quantidade: number }[],
-        totalPedido: number
-    }> {
-        const promises = produtos.map(async ({ id, quantidade }) => {
+    private async _formatarProdutosPedido(produtos: { id: string, quantidade: number }[]): Promise<{ produto: Produto, quantidade: number }[]> {
+        const produtosPromises = produtos.map(async ({ id, quantidade }) => {
             const produto = await this.produtoGateway.buscarProdutoPorId(id);
             if (!produto) {
                 throw new Error('Produto não encontrado')
             }
-            const total = produto.preco * quantidade
-            return { produto, total, quantidade };
+            return { produto: new Produto(produto.id, produto.categoria, produto.nome, produto.preco, produto.descricao), quantidade }
         });
 
-        const produtosPedido = await Promise.all(promises);
-        const totalPedido = produtosPedido.reduce((t, { total }) => t + total, 0);
+        return Promise.all(produtosPromises);
+    }
 
-        return { produtosPedido, totalPedido }
+    private _calcularTotalPedido(produtosPedido: { produto: Produto, quantidade: number }[]): number {
+        return produtosPedido.reduce((totalPedido, { produto, quantidade }) => totalPedido + (produto.preco * quantidade), 0);
     }
 }
