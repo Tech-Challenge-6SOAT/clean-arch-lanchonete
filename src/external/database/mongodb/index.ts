13b680ea4a0c6b867531a4c230d9d17ea396c0de
Danilo Cassola <danilocassola@gmail.com>
2024-07-19 18:38:03 -0300

    chore: add logger to database mongoose connection

--- src/external/database/mongodb/index.ts
+++ src/external/database/mongodb/index.ts
@@ -1,25 +1,24 @@
-// import { Logger } from "../../logs/logger"
+import { Logger } from "../../logs/logger"
 import mongoose from "mongoose"
 
 const connection = () => {
-  // const mongoUsername = process.env.MONGODB_USERNAME
-  // const mongoPassword = process.env.MONGODB_PASSWORD
-  // const mongoHost = process.env.MONGODB_HOST
-  // const mongoDatabase = process.env.MONGODB_DATABASE
-  // const url = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}/${mongoDatabase}?retryWrites=true&w=majority`
-  const url = `mongodb://admin:admin@localhost:27017/lanchonete?retryWrites=true&w=majority`
+  const mongoUsername = 'admin'
+  const mongoPassword = 'admin'
+  const mongoHost = 'localhost:27017'
+  const mongoDatabase = 'lanchonete'
+  const url = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}/${mongoDatabase}?retryWrites=true&w=majority`
   const conn = mongoose.createConnection(url)
 
   conn.once('connected', () => {
-    console.info('MongoDB is connected!')
+    Logger.info('MongoDB is connected!')
   })
 
   conn.on('error', (err: Error) => {
-    console.error(`MongoDBconnection error: ${err.message}`)
+    Logger.error(`MongoDBconnection error: ${err.message}`)
   })
 
   conn.on('disconnected', () => {
-    console.error('MongoDB disconnected')
+    Logger.error('MongoDB disconnected')
   })
 
   return conn
