4654084d2942502dbe4f175ab4f8d0c8ccadc07b
Thiago Nascimbeni <tnascimbeni@gmail.com>
2024-07-27 22:04:47 -0300

    add webhook validation

--- src/api/routes/webhook/index.ts
+++ src/api/routes/webhook/index.ts
@@ -4,18 +4,31 @@ import { TransacaoDbConnection } from "../../../external/database/mongodb/db-con
 import { PagamentoUseCase } from "../../../usecases";
 import { PlataformaPagamentoFake } from "../../../external/pagamento/plataformaPagamentoFake";
 import { WebhookController } from "../../../controllers/webhook";
+import { atualizarStatusPagamentoSchema } from "./schema";
+import { validatorCompiler } from "../../validators/ajv";
 
 export const webhookRoutes = async (app: FastifyInstance) => {
-  
-  const transacaoDbConnection = new TransacaoDbConnection()
-  const transacaoGateway = new TransacaoGateway(transacaoDbConnection)
-  const plataformaPagamentoFake = new PlataformaPagamentoFake()
-  const pagamentoGateway = new PagamentoGateway(plataformaPagamentoFake)
-  const pagamentoUseCase = new PagamentoUseCase(transacaoGateway, pagamentoGateway)
-  const webhookController = new WebhookController(pagamentoUseCase)
+  const transacaoDbConnection = new TransacaoDbConnection();
+  const transacaoGateway = new TransacaoGateway(transacaoDbConnection);
+  const plataformaPagamentoFake = new PlataformaPagamentoFake();
+  const pagamentoGateway = new PagamentoGateway(plataformaPagamentoFake);
+  const pagamentoUseCase = new PagamentoUseCase(
+    transacaoGateway,
+    pagamentoGateway
+  );
+  const webhookController = new WebhookController(pagamentoUseCase);
 
-  app.post('/webhook/pagamento', {}, async function (request, reply) {
-    const response = await webhookController.atualizarStatusPagamento(request)
-    return reply.status(response.statusCode).send(response.data)
-  })
-}
+  app.post(
+    "/webhook/pagamento",
+    {
+      schema: atualizarStatusPagamentoSchema,
+      validatorCompiler,
+    },
+    async function (request, reply) {
+      const response = await webhookController.atualizarStatusPagamento(
+        request
+      );
+      return reply.status(response.statusCode).send(response.data);
+    }
+  );
+};
---
+++ src/api/routes/webhook/schema.ts
@@ -0,0 +1,27 @@
+export const atualizarStatusPagamentoSchema = {
+  tags: ["pedido"],
+  body: {
+    type: "object",
+    properties: {
+      idTransacaoExterna: { type: "string" },
+      pagamentoStatus: {
+        type: "object",
+        properties: {
+          status: { type: "string" },
+        },
+        required: ["status"],
+      },
+    },
+    required: ["idTransacaoExterna", "pagamentoStatus"],
+  },
+  response: {
+    201: {
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        senha: { type: "string" },
+        qrcode: { type: "string" },
+      },
+    },
+  },
+};
