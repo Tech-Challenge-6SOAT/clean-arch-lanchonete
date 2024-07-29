e19d272e6670bbb3e8d869fdb24e76dfcf50e262
Danilo Cassola <danilocassola@gmail.com>
2024-07-28 22:09:57 -0300

    chore: add statusEnum to schema validate

--- src/api/routes/webhook/schema.ts
+++ src/api/routes/webhook/schema.ts
@@ -1,10 +1,18 @@
+const statusEnum = ["paid", "refused", "waiting_payment"];
+
 export const atualizarStatusPagamentoSchema = {
   tags: ["webhook"],
   body: {
     type: "object",
     properties: {
       id: { type: "string" },
-      status: { type: "string" },
+      status: {
+        type: "string",
+        enum: statusEnum,
+        errorMessage: {
+          enum: `status should be one of ${statusEnum.join(', ')}`
+        }
+      }
     },
     required: ["id", "status"],
   },
